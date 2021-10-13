444edbc07bda7f6e41d16756adaec76501ec7f05
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-10-12 23:36:10 -0300

    Implementacion de Scroll Reveal JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.mySwiper', {
+let swiper = new Swiper('.mySwiper', {
 	grabCursor: true,
 	effect: 'creative',
 	creativeEffect: {
@@ -14,4 +14,22 @@ var swiper = new Swiper('.mySwiper', {
 	pagination: {
 		el: '.swiper-pagination',
 	},
+})
+
+ScrollReveal().reveal('.about-us__info-one', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '-100px',
+})
+
+ScrollReveal().reveal('.about-us__info-two', {
+	duration: 2000,
+	origin: 'right',
+	distance: '-100px',
+})
+
+ScrollReveal().reveal('.about-us__info-three', {
+	duration: 2000,
+	origin: 'top',
+	distance: '-100px',
 })
