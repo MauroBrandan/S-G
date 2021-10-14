695835638cd0aa8b3e49a6952139a414a2c46830
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-10-14 13:44:43 -0300

    Arreglos en el responsive Agregar solo Swiper en la version mobile

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-let swiper = new Swiper('.mySwiper', {
-	grabCursor: true,
-	effect: 'creative',
-	creativeEffect: {
-		prev: {
-			shadow: true,
-			translate: ['-120%', 0, -500],
+let body = document.querySelector('body')
+let servicesWrapper = document.querySelector('.services__cards-wrapper')
+let servicesCard = document.querySelectorAll('.services__card')
+let sliderWrapper = document.querySelector('.slider-wrapper')
+
+if (body.clientWidth <= 1005) {
+	servicesWrapper.classList.add('swiper', 'mySwiper')
+
+	for (let i = 0; i < servicesCard.length; i++) {
+		servicesCard[i].classList.add('swiper-slide')
+	}
+
+	sliderWrapper.classList.add('swiper-wrapper')
+
+	let swiper = new Swiper('.mySwiper', {
+		grabCursor: true,
+		effect: 'creative',
+		creativeEffect: {
+			prev: {
+				shadow: true,
+				translate: ['-120%', 0, -500],
+			},
+			next: {
+				shadow: true,
+				translate: ['120%', 0, -500],
+			},
 		},
-		next: {
-			shadow: true,
-			translate: ['120%', 0, -500],
+		pagination: {
+			el: '.swiper-pagination',
 		},
-	},
-	pagination: {
-		el: '.swiper-pagination',
-	},
-})
+	})
+}
 
 ScrollReveal().reveal('.about-us__info-one', {
 	duration: 2000,
